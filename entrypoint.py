0fa3a4b6b5a16d64ada1d2def155f42ff0f442d2
0x4448 <86135470+0x4448@users.noreply.github.com>
2023-07-08 11:32:59 -0700

    Check for GITHUB_TOKEN

--- entrypoint.py
+++ entrypoint.py
@@ -4,8 +4,17 @@ import os
 import sys
 
 from datetime import datetime
+from pathlib import Path
 
 print(f"Hello {sys.argv[1]}")
+
+token = Path("/run/secrets/github_token")
+print(f"token: {token.exists()}")
+
+if token.exists():
+    with open(token) as fp:
+        line = fp.readline()
+        print(line[0:4])
 
 with open(os.environ["GITHUB_OUTPUT"], "a") as fp:
     fp.write(f"out-var1=The time is {datetime.now()}")
