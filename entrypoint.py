abb8a368721573e48b5a4cd9367cc339dcabae25
0x4448 <86135470+0x4448@users.noreply.github.com>
2023-07-08 10:55:56 -0700

    Try appending to GITHUB_OUTPUT

--- entrypoint.py
+++ entrypoint.py
@@ -7,5 +7,5 @@ from datetime import datetime
 
 print(f"Hello {sys.argv[1]}")
 
-with open(os.environ["GITHUB_OUTPUT"]) as fp:
+with open(os.environ["GITHUB_OUTPUT"], "a") as fp:
     fp.write(f"The time is {datetime.now()}")
