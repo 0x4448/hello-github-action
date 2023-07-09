26a4d0b91ccff0fd32ed8a6d199ca286aa5d3ee1
0x4448 <86135470+0x4448@users.noreply.github.com>
2023-07-09 08:51:14 -0700

    Switch to composite action

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-# Container image that runs your code
-FROM python:3.11
-
-# Copies your code file from your action repository to the filesystem path `/` of the container
-COPY entrypoint.py /entrypoint.py
-
-# Code file to execute when the docker container starts up (`entrypoint.sh`)
-ENTRYPOINT ["/entrypoint.py"]
--- entrypoint.py
+++ entrypoint.py
@@ -8,5 +8,8 @@ from datetime import datetime
 print(f"Hello {sys.argv[1]}")
 print(f"GITHUB_TOKEN length: {len(os.environ.get('GITHUB_TOKEN', ''))}")
 
+for key in sorted(os.environ.keys()):
+    print(key)
+
 with open(os.environ["GITHUB_OUTPUT"], "a") as fp:
     fp.write(f"out-var1=The time is {datetime.now()}")
